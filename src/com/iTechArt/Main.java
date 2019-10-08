306132e3b9975b709635ce4f07f4b73967ad7955
Uladzislau Hryhoryeu <Uladzislau.Hryhoryeu@itechart-group.com>
2019-10-08 15:09:02 +0300

    Initial commit

---
+++ src/com/iTechArt/Main.java
@@ -0,0 +1,11 @@
+package com.iTechArt;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println(3+22);
+        System.out.println("3"+22);
+    }
+}
